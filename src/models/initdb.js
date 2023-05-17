0d902bc829bc813edb763a2ef380cdebc1ab6d90
willianstars <willian.15102017@gmail.com>
2023-05-16 21:51:55 -0400

    update

--- src/models/initdb.js
+++ src/models/initdb.js
@@ -14,13 +14,16 @@ mariadb.createConnection({
     conexion.query("CREATE TABLE IF NOT EXISTS " + env.DATABASE + ".persona (id INT(11) PRIMARY KEY AUTO_INCREMENT, tipo_persona varchar(20) not null, nombres VARCHAR(64) not null, apellidos VARCHAR(64) not null, tipo_documento varchar(20) not null, num_documento varchar(20) not null, direccion varchar(70) not null, telefono varchar(20) not null, email VARCHAR(128) not null UNIQUE, password VARCHAR(128) not null ) ENGINE=InnoDB ", (errordtb) => {
         if(errordtb) throw errordtb
     })
-    conexion.query("create table if not exists" + env.DATABASE + ".evento(id int(11) primary key not null auto_increment, Id_evento int(11) not null, evento varchar(64) not null, créate_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=InnoDB", (errordtb) => {        
+    conexion.query("create table if not exists" + env.DATABASE + ".evento(id int(11) primary key not null auto_increment, id_evento int(11) not null, evento varchar(64) not null, create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=InnoDB", (errordtb) => {        
         if(errordtb) throw errordtb
     })
     conexion.query("create table if not exists" + env.DATABASE + ".detalle_venta(id_detal_venta int(11) primary key not null auto_increment, Id_articulo int(11) not null, cantidad int(11) not null, precio decimal(11,2) not null, descuento decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {        
         if(errordtb) throw errordtb
     })
     conexion.query("create table if not exists" + env.DATABASE + ".articulo(id_articulo int(11) primary key not null auto_increment, id_categoria int(11) not null, codigo varchar(50) null, nombre varchar(100) not null unique, costo decimal(11,2) not null, precio_venta decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {        
+        if(errordtb) throw errordtb
+    })
+    conexion.query("create table if not exists" + env.DATABASE + ".bodega(id_bodega int(11) primary key not null auto_increment, id_articulo int(11) not null, cantidad int(11) not null) ENGINE=InnoDB", (errordtb) => {        
         if(errordtb) throw errordtb
     })
 }).catch((error) => {
