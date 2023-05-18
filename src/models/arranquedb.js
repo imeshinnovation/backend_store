612d9940f0603c47da19814f837f4d85143982ac
willianstars <willian.15102017@gmail.com>
2023-05-17 22:32:11 -0400

    Update

--- src/models/arranquedb.js
+++ src/models/arranquedb.js
@@ -17,18 +17,18 @@ mariadb.createConnection({
     conexion.query("create table if not exists " + env.DATABASE + ".log(id int(11) primary key not null auto_increment, id_evento int(11) not null, evento varchar(64) not null, create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=InnoDB", (error) => {        
         if(error) throw error
     })
-    //conexion.query("create table if not exists " + env.DATABASE + ".detalle_venta(id_detal_venta int(11) primary key not null auto_increment, Id_articulo int(11) not null, cantidad int(11) not null, precio decimal(11,2) not null, descuento decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {        
-    //    if(errordtb) throw errordtb
-    //})
-    //conexion.query("create table if not exists " + env.DATABASE + ".articulo(id_articulo int(11) primary key not null auto_increment, id_categoria int(11) not null, codigo varchar(50) null, nombre varchar(100) not null unique, costo decimal(11,2) not null, precio_venta decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {
-    //    if(errordtb) throw errordtb
-    //})
-    //conexion.query("create table if not exists " + env.DATABASE + ".bodega(id_bodega int(11) primary key not null auto_increment, id_articulo int(11) not null, cantidad int(11) not null) ENGINE=InnoDB", (errordtb) => {        
-    //    if(errordtb) throw errordtb
-    //})
+    conexion.query("create table if not exists " + env.DATABASE + ".detalle_venta(id_detal_venta int(11) primary key not null auto_increment, Id_articulo int(11) not null, cantidad int(11) not null, precio decimal(11,2) not null, descuento decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {        
+        if(errordtb) throw errordtb
+    })
+    conexion.query("create table if not exists " + env.DATABASE + ".articulo(id_articulo int(11) primary key not null auto_increment, id_categoria int(11) not null, codigo varchar(50) null, nombre varchar(100) not null unique, costo decimal(11,2) not null, precio_venta decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {
+        if(errordtb) throw errordtb
+    })
+    conexion.query("create table if not exists " + env.DATABASE + ".bodega(id_bodega int(11) primary key not null auto_increment, id_articulo int(11) not null, cantidad int(11) not null) ENGINE=InnoDB", (errordtb) => {        
+        if(errordtb) throw errordtb
+    })
 
     //actuaizaciones registradas en evento de bodega
-    //conexion.query("DELIMITER $$ CREATE trigger insert_bodega_event AFTER insert on bodega for each row BEGIN SET @dato = new.articulo; insert into log (evento) value(CONCAT('se creo un evento en la tabla bodega: ', @dato)); end $$ ")
+    conexion.query("DELIMITER $$ CREATE trigger insert_bodega_event AFTER insert on bodega for each row BEGIN SET @dato = new.articulo; insert into log (evento) value(CONCAT('se creo un evento en la tabla bodega: ', @dato)); end $$ ")
     //conexion.query("DELIMITER $$ CREATE trigger del_bodega_event AFTER delete on bodega for each row BEGIN SET @dato = old.articulo; insert into log (evento) value(CONCAT('se elimino un articulo en la tabla bodega: ', @dato)); end $$ ")
 
     //actualizaciones registradas en evento de persona
