6b8b17b4ac342d0b1890c43e95178b64034a42c2
willianstars <willian.15102017@gmail.com>
2023-05-30 22:36:29 -0400

    up

--- src/controladores/articulos.js
+++ src/controladores/articulos.js
@@ -2,7 +2,7 @@ const querys = require('../librerias/querys')
 
 module.exports = {
   arttototal:  async () =>{
-    return await querys("SELECT * FROM articulos")
+    return await querys("SELECT * FROM articulo")
   },
 
   artunitario:  async () =>{
@@ -10,13 +10,15 @@ module.exports = {
   },
 
   addarticulo: async (data) =>{
-    try{
-        const{nombre, costo, precio_venta} = data
-        const newarticulo = await querys(`INSERT INTO articulo (nombre, costo, precio_venta) VALUES ("${nombre}", "${costo}", "${precio_venta}"`)
+    //try{
+        const{id_categoria, nombre, costo, precio_venta} = data
+        const newarticulo = await querys(`INSERT INTO articulo (id_categoria, nombre, costo, precio_venta) VALUES ("${id_categoria}","${nombre}", "${costo}", "${precio_venta}"`)
+        console.log(newarticulo)
         return newarticulo.affectedRows > 0 ? {'code': 'articulo ingresado'} : {'code': 'Articulo ya se encuentra registrado'}
-    } catch (err){
-        return {'code': err.text}
-    }
+    //} catch (err){
+    //  console.log(err.text)  
+    //  return {'code': err}
+    //}
 },
   delarticulo: async (data) =>{
   try{
--- src/rutas/users.js
+++ src/rutas/users.js
@@ -2,6 +2,20 @@ const Rutas = require('express').Router()
 
 const usuarios = require('../controladores/personas')
 const ventas = require('../controladores/ventas')
+const articulos = require('../controladores/articulos')
+
+//articulos.js
+Rutas.get('/artt', async (req, res) => {
+    res.json(await articulos.arttototal(req.params.arttototal))
+})
+Rutas.get('/artu', async (req, res) => {
+    res.json(await articulos.artunitario(req.params.artunitario))
+})  
+
+Rutas.post('/artadd', async (req, res) => {
+    res.json(await articulos.addarticulo(req.body))
+})
+
 
 //ventas.js
 Rutas.get('/ventat', async (req, res) => {
@@ -22,7 +36,7 @@ Rutas.post('/agregar', async (req, res) => {
 
 
 Rutas.post('/login', async (req,res) => {
-    res.json(await usuarios.login(req.body))
+    res.json(await usuarios.agregar(req.body))
 })
 
 // read a unique user
