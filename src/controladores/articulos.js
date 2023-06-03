f57f5770bb0423f51a188103869fc3ecb20de8ca
willianstars <willian.15102017@gmail.com>
2023-06-02 22:20:05 -0400

    update

--- src/controladores/articulos.js
+++ src/controladores/articulos.js
@@ -12,7 +12,7 @@ module.exports = {
   addarticulo: async (data) =>{
     //try{
         const{id_categoria, nombre, costo, precio_venta} = data
-        const newarticulo = await querys(`INSERT INTO articulo (id_categoria, nombre, costo, precio_venta) VALUES (${id_categoria}, "${nombre}", ${costo}, ${precio_venta})`)
+        const newarticulo = await querys(`INSERT INTO articulo (id_categoria, id_categoria, nombre, costo, precio_venta) VALUES (${id_categoria}, ${id_categoria}, "${nombre}", ${costo}, ${precio_venta})`)
         console.log(newarticulo)
         return newarticulo.affectedRows > 0 ? {'code': 'articulo ingresado'} : {'code': 'Articulo ya se encuentra registrado'}
     //} catch (err){
--- src/models/arranquedb.js
+++ src/models/arranquedb.js
@@ -20,6 +20,9 @@ mariadb.createConnection({
     conexion.query("create table if not exists " + env.DATABASE + ".articulo(id_articulo int(11) primary key not null auto_increment, id_categoria int(11) not null, codigo varchar(50) null, nombre varchar(100) not null unique, costo decimal(11,2) not null, precio_venta decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {
         if(errordtb) throw errordtb
     })
+    conexion.query("create table if not exists " + env.DATABASE + ".categoria(id_categoria int(11) primary key not null auto_increment, id_categoria int(11) not null, nombre varchar(100) not null unique) ENGINE=InnoDB", (errordtb) => {
+        if(errordtb) throw errordtb
+    })
     conexion.query("create table if not exists " + env.DATABASE + ".detalle_venta(id_detal_venta int(11) primary key not null auto_increment, Id_articulo int(11) not null, cantidad int(11) not null, precio decimal(11,2) not null, descuento decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {        
         if(errordtb) throw errordtb
     })
