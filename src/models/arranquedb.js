9f17d3853f9118b871e60abd30ba2559187642ec
willianstars <91298359+willianstars@users.noreply.github.com>
2023-06-06 13:42:26 -0400

    Update arranquedb.js

--- src/models/arranquedb.js
+++ src/models/arranquedb.js
@@ -17,10 +17,7 @@ mariadb.createConnection({
     conexion.query("create table if not exists " + env.DATABASE + ".log(id int(11) primary key not null auto_increment, id_evento int(11) not null, evento varchar(64) not null, create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=InnoDB", (error) => {        
         if(error) throw error
     })
-    conexion.query("create table if not exists " + env.DATABASE + ".articulo(id int(11) primary key not null auto_increment, id_categoria int(11) not null, codigo varchar(50) null, nombre varchar(100) not null unique, costo decimal(11,2) not null, precio_venta decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {
-        if(errordtb) throw errordtb
-    })
-    conexion.query("create table if not exists " + env.DATABASE + ".categoria(id_categoria int(11) primary key not null auto_increment, nombre varchar(100) not null unique) ENGINE=InnoDB", (errordtb) => {
+    conexion.query("create table if not exists " + env.DATABASE + ".articulo(id int(11) primary key not null auto_increment, codigo varchar(50) not null, nombre varchar(100) not null unique, costo decimal(11,2) not null, precio_venta decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {
         if(errordtb) throw errordtb
     })
     conexion.query("create table if not exists " + env.DATABASE + ".detalle_venta(id_detal_venta int(11) primary key not null auto_increment, Id_articulo int(11) not null, cantidad int(11) not null, precio decimal(11,2) not null, descuento decimal(11,2) not null) ENGINE=InnoDB", (errordtb) => {        
@@ -29,24 +26,6 @@ mariadb.createConnection({
     conexion.query("create table if not exists " + env.DATABASE + ".bodega(id_bodega int(11) primary key not null auto_increment,categoria varchar(64) not null, id_articulo int(11) not null, cantidad int(11) not null) ENGINE=InnoDB", (errordtb) => {        
         if(errordtb) throw errordtb
     })
-
-    //actuaizaciones registradas en evento de bodega
-    //conexion.query(`DELIMITER $$ CREATE trigger insert_bodega_event AFTER insert on ${env.DATABASE}.bodega for each row BEGIN SET @dato = new.articulo; insert into log (evento) value(CONCAT('se creo un evento en la tabla bodega: ', @dato)); end $$`)
-    //conexion.query("DELIMITER $$ CREATE trigger del_bodega_event AFTER delete on bodega for each row BEGIN SET @dato = old.articulo; insert into log (evento) value(CONCAT('se elimino un articulo en la tabla bodega: ', @dato)); end $$ ")
-
-    //actualizaciones registradas en evento de persona
-    //conexion.query("DELIMITER $$ CREATE trigger insert_person_event AFTER insert on persona for each row BEGIN SET @dato = new.persona; insert into log (evento) value(CONCAT('se creo un evento en la tabla persona: ', @dato)); end $$ ")
-    // conexion.query("DELIMITER $$ CREATE trigger del_person_event AFTER delete on persona for each row BEGIN SET @dato = old.persona; insert into log (evento) value(CONCAT('se elimino un usuario en la tabla persona: ', @dato)); end $$ ")
-
-    //intento de margen de ganancia
-    //conexion.query("DELIMITER $$ CREATE trigger porcentaje_ganancia BEFORE insert on articulos for each row BEGIN SET @costo_venta = (((NEW.costo * NEW.ganancia) / 100) + NEW.costo) SET NEW.costo_venta = @costovento end $$")
-    
-    //const modifyTriggerQuery = `
-    //DROP TRIGGER IF EXISTS `NuevoPrecioVentaAfter`  DELIMITER && CREATE TRIGGER  `NuevoPrecioVentaAfter` AFTER INSERT ON `articulo` FOR EACH ROW BEGIN INSERT INTO `log`  (id_evento, Evento) VALUES (NEW.id, CONCAT('Se Creo el Producto: ', NEW.Nombre_Producto)); END&& DELIMITER ;
-//`;
-
-    //conexion.query(`DROP TRIGGER IF EXISTS `/"NuevoPrecioVentaAfter"/`  DELIMITER && CREATE TRIGGER  `/"NuevoPrecioVentaAfter"/` AFTER INSERT ON `/"articulo"/` FOR EACH ROW BEGIN INSERT INTO `/"log"/` (id_evento, Evento) VALUES (NEW.id, CONCAT('Se Creo el Producto: ', NEW.Nombre_Producto)); END&& DELIMITER ;`)
-
 }).catch((error) => {
     console.log('mira, aqui la regaste -> ', error)
 })
